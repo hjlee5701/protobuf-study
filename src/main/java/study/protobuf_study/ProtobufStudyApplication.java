d106754dfba217096037b74bbb952dc30b7a6b64
hjlee5701 <hjlee5701@gmail.com>
2024-11-14 15:47:44 +0900

    Initial commit

---
+++ src/main/java/study/protobuf_study/ProtobufStudyApplication.java
@@ -0,0 +1,13 @@
+package study.protobuf_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/protobuf_study/ProtobufStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.protobuf_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
